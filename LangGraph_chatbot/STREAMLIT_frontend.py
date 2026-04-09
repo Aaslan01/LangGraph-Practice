b6bcec7dd33518d7166fc70a1a4a6f84807d2af5
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2026-04-08 20:36:17 -0400

    Updated Session Memory

--- LangGraph_chatbot/STREAMLIT_frontend.py
+++ LangGraph_chatbot/STREAMLIT_frontend.py
@@ -2,12 +2,31 @@ import streamlit as st
 
 st.title("Simple Chat Interface")
 
+if 'memory_history' not in st.session_state:
+    st.session_state['memory_history'] = []
+
+
+
+
+if st.session_state['memory_history']:
+    for message in st.session_state['memory_history']:
+        with st.chat_message(message["role"]):
+            st.text(message["content"])
 
 # Assistant message block
-st.chat_message("assistant").write("Hello! How can I help you today?")
+# show input text box at the bottom of the page
+user_input = st.chat_input("Enter your message:")
 
-
-# User message block
-user_message = st.chat_input("Type your message...")
-if user_message:
-    st.chat_message("user").write(user_message)
+if user_input:
+    
+    # Append user message to memory history
+    st.session_state['memory_history'].append({"role": "user", "content": user_input})
+    # Display user message
+    with st.chat_message("user"):
+        st.text(user_input)
+        
+    #append assistant message to memory history
+    st.session_state['memory_history'].append({"role": "assistant", "content": user_input})
+     # Display assistant message
+    with st.chat_message("assistant"):
+        st.text(user_input)
